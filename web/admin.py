138f9d8431767b7c5467ff90408a9a493bb6f574
alisssaaaddd <ali.sadeghi.m@ut.ac.ir>
2024-12-13 14:33:56 +0330

    Add jalali datetime succesfully

--- web/admin.py
+++ web/admin.py
@@ -1,5 +1,31 @@
 from django.contrib import admin
+from django import forms
+from django_jalali.forms import jDateField, jDateInput
+from django_jalali.admin.widgets import AdminjDateWidget
+from django_jalali.admin.filters import JDateFieldListFilter
+
 from .models import Expense
+from django_jalali.db import models as jmodels
 
 
-admin.site.register(Expense)
+class ExpenseForm(forms.ModelForm):
+    date = jDateField(widget=AdminjDateWidget) 
+
+    class Meta:
+        model = Expense
+        fields = '__all__'
+
+
+
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = ['text', 'amount', 'date', 'user']
+    list_filter = (('date', JDateFieldListFilter),) 
+    form = ExpenseForm 
+
+
+    formfield_overrides = {
+        jmodels.jDateField: {'widget': AdminjDateWidget},  
+    }
+
+
+admin.site.register(Expense, ExpenseAdmin)
--- web/models.py
+++ web/models.py
@@ -1,13 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django_jalali.db import models as jmodels
+
+
 
 DEFAULT_USER_ID = 1  # Ensure this matches the default user's ID
 
 class Expense(models.Model):
     text = models.CharField(max_length=255)
-    date = models.DateTimeField()
+    date = jmodels.jDateField()
     amount = models.BigIntegerField()
     user = models.ForeignKey(User, on_delete=models.CASCADE, default=DEFAULT_USER_ID)
 
     def __str__(self):
         return f"{self.user.username}'s expense of {self.amount} on {self.date}"
+
+
